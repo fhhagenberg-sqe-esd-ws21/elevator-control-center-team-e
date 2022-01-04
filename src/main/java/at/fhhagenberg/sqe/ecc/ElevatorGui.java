5de7d4b8132cdd76531cdce30e8a5712f82e552f
Wandl Bernhard - S2010567008 <S2010567008@fhooe.at>
2022-01-04 17:39:40 +0100

    continue implementing GUI, also testing GUI

--- src/main/java/at/fhhagenberg/sqe/ecc/ElevatorGui.java
+++ src/main/java/at/fhhagenberg/sqe/ecc/ElevatorGui.java
@@ -2,8 +2,11 @@ package at.fhhagenberg.sqe.ecc;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
@@ -16,66 +19,88 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
-public class ElevatorGui implements EventHandler{
-	
+public class ElevatorGui implements EventHandler {
+
 	private ElevatorModel model;
-	
-	@FXML private HBox elevators;
-	@FXML private VBox elevatorButtons;
-	@FXML private VBox callButtons;
-	
+
+	@FXML
+	private HBox elevators;
+	@FXML
+	private VBox callButtons;
+
 	@Override
 	public void handle(Event event) {
 		Node node = (Node) event.getSource();
 		int elevator = new Scanner(node.getId()).useDelimiter("\\D+").nextInt();
-		
+
 		if (node.getStyleClass().contains("auto")) {
 			Node btnManual = elevators.lookup("#buttonManual" + Integer.toString(elevator));
-			btnManual.setDisable(true);
+			btnManual.setDisable(false);
+			node.setDisable(true);
+		} else if (node.getStyleClass().contains("manual")) {
+			Node btnAuto = elevators.lookup("#buttonAuto" + Integer.toString(elevator));
+			btnAuto.setDisable(false);
+			node.setDisable(true);
 		}
-		else if (node.getStyleClass().contains("manual")) {
-			System.out.println("Test2");
-		}
-		
+
 	}
-	
+
 	private AnchorPane ConstructElevator(int number) {
-				
+
 		Label title = new Label("Elevator " + Integer.toString(number));
 		Button buttonManual = new Button("Manual");
 		buttonManual.setId("buttonManual" + Integer.toString(number));
 		buttonManual.getStyleClass().add("manual");
 		buttonManual.setOnAction(this);
-		
+
 		Button buttonAuto = new Button("Auto");
 		buttonAuto.setId("buttonAuto" + Integer.toString(number));
 		buttonAuto.getStyleClass().add("auto");
 		buttonAuto.setOnAction(this);
-		
+
+		buttonManual.setDisable(true);
+
 		VBox elevatorButtons = new VBox();
-		
-		for(int i = model.getNumOfFloors()-1; i >= 0; --i) {
+		elevatorButtons.getStyleClass().add("elevator-floors");
+
+		for (int i = model.getNumOfFloors() - 1; i >= 0; --i) {
 			elevatorButtons.getChildren().add(new Label(Integer.toString(i)));
-        }
-		
+		}
+
 		Label labelPosition = new Label("Position: ");
+		labelPosition.getStyleClass().add("position-label");
 		Label labelSpeed = new Label("Speed: ");
-		Label labelPayload = new Label("Payload: ");
+		labelSpeed.getStyleClass().add("speed-label");
+		Label labelAccel = new Label("Accel: ");
+		labelAccel.getStyleClass().add("accel-label");
 		Label labelDoors = new Label("Doors: ");
+		labelDoors.getStyleClass().add("doors-label");
 		Label labelTarget = new Label("Target: ");
-		
+		labelTarget.getStyleClass().add("target-label");
+		Label labelFloor = new Label("Floor: ");
+		labelTarget.getStyleClass().add("floor-label");
+		Label labelWeight = new Label("Weight: ");
+		labelWeight.getStyleClass().add("weight-label");
+		Label labelCapacity = new Label("Capacity: ");
+		labelCapacity.getStyleClass().add("capacity-label");
+
 		VBox status = new VBox();
 		status.getChildren().add(labelPosition);
 		status.getChildren().add(labelSpeed);
-		status.getChildren().add(labelPayload);
+		status.getChildren().add(labelAccel);
 		status.getChildren().add(labelDoors);
 		status.getChildren().add(labelTarget);
-		
+		status.getChildren().add(labelFloor);
+		status.getChildren().add(labelWeight);
+		status.getChildren().add(labelCapacity);
+
 		Label labelUp = new Label("UP");
+		labelUp.getStyleClass().add("btn-up");
 		Label labelDown = new Label("DOWN");
-		
-		
+		labelDown.getStyleClass().add("btn-down");
+
 		VBox content = new VBox();
 		content.getChildren().add(title);
 		content.getChildren().add(buttonManual);
@@ -84,29 +109,172 @@ public class ElevatorGui implements EventHandler{
 		content.getChildren().add(elevatorButtons);
 		content.getChildren().add(labelDown);
 		content.getChildren().add(status);
-		
+
 		AnchorPane pane = new AnchorPane(content);
 		pane.setId("Elevator" + Integer.toString(number)); // #Elevator1
-		
+
 		return pane;
 	}
-	
-	@FXML 
+
+	@FXML
 	public void InitGui() {
-		
-		for(int i = model.getNumOfFloors()-1; i >= 0; --i) {
-        	callButtons.getChildren().add(new Label(Integer.toString(i)));
-        }
-		
-		for(int i = 0; i < model.getNumOfElevators(); ++i) {
-        	AnchorPane pane = this.ConstructElevator(i);
-        	elevators.getChildren().add(pane);
-        }
+
+		for (int i = model.getNumOfFloors() - 1; i >= 0; --i) {
+			HBox floor = new HBox();
+			floor.setId("floor" + Integer.toString(i));
+			Label up = new Label("UP");
+			up.getStyleClass().add("btn-up");
+			up.setMinWidth(20);
+			Label down = new Label("DOWN");
+			down.getStyleClass().add("btn-down");
+			down.setMinWidth(50);
+
+			floor.getChildren().add(up);
+			floor.getChildren().add(new Label(Integer.toString(i)));
+			floor.getChildren().add(down);
+
+			callButtons.getChildren().add(floor);
+		}
+
+		for (int i = 0; i < model.getNumOfElevators(); ++i) {
+			AnchorPane pane = this.ConstructElevator(i);
+			elevators.getChildren().add(pane);
+		}
+
 	}
 
 	public void setModel(ElevatorModel model) {
 		this.model = model;
-		
+
+	}
+
+	public void setCommitedDirection(int elevator, int direction) {
+
+		Node btnUp = elevators.lookup("#Elevator" + Integer.toString(elevator) + " .btn-up");
+		btnUp.setStyle("-fx-text-fill: black");
+		Node btnDown = elevators.lookup("#Elevator" + Integer.toString(elevator) + " .btn-down");
+		btnDown.setStyle("-fx-text-fill: black");
+
+		switch (direction) {
+		case IElevator.ELEVATOR_DIRECTION_DOWN:
+			btnDown.setStyle("-fx-text-fill: red");
+			break;
+		case IElevator.ELEVATOR_DIRECTION_UP:
+			btnUp.setStyle("-fx-text-fill: red");
+			break;
+		default:
+			break;
+		}
+	}
+
+	public void setTargetFloor(int elevator, int floor) {
+		Label label = (Label) elevators.lookup("#Elevator" + Integer.toString(elevator) + " .target-label");
+		Platform.runLater(() -> {
+			label.setText("Target: " + Integer.toString(floor));
+		});
+	}
+
+	public void setElevatorAccel(int elevator, int value) {
+		Label label = (Label) elevators.lookup("#Elevator" + Integer.toString(elevator) + " .accel-label");
+		Platform.runLater(() -> {
+			label.setText("Accel: " + Integer.toString(value));
+		});
+	}
+
+	public void setElevatorButton(int elevator, int floor, boolean pressed) {
+		VBox floorBox = (VBox) elevators.lookup("#Elevator" + Integer.toString(elevator) + " .elevator-floors");
+		ObservableList<Node> floorList = floorBox.getChildren();
+		Label lblFloor = (Label) floorList.get(floorList.size() - 1 - floor);
+
+		if (pressed) {
+			lblFloor.setTextFill(Color.GREEN);
+		} else {
+			lblFloor.setTextFill(Color.BLACK);
+		}
+	}
+
+	public void setElevatorDoorStatus(int elevator, int status) {
+		Label label = (Label) elevators.lookup("#Elevator" + Integer.toString(elevator) + " .doors-label");
+
+		final String statusText;
+
+		switch (status) {
+		case IElevator.ELEVATOR_DOORS_CLOSED:
+			statusText = "CLOSED";
+			break;
+		case IElevator.ELEVATOR_DOORS_CLOSING:
+			statusText = "CLOSING";
+			break;
+		case IElevator.ELEVATOR_DOORS_OPEN:
+			statusText = "OPEN";
+			break;
+		case IElevator.ELEVATOR_DOORS_OPENING:
+			statusText = "OPENING";
+			break;
+		default:
+			statusText = "";
+			break;
+		}
+
+		Platform.runLater(() -> {
+			label.setText("Doors: " + statusText);
+		});
+	}
+
+	public void setElevatorFloor(int elevator, int floor) {
+		Label label = (Label) elevators.lookup("#Elevator" + Integer.toString(elevator) + " .floor-label");
+		Platform.runLater(() -> {
+			label.setText("Floor: " + Integer.toString(floor));
+		});
+	}
+
+	public void setElevatorPosition(int elevator, int value) {
+		Label label = (Label) elevators.lookup("#Elevator" + Integer.toString(elevator) + " .position-label");
+		Platform.runLater(() -> {
+			label.setText("Position: " + Integer.toString(value));
+		});
+	}
+
+	public void setElevatorSpeed(int elevator, int value) {
+		Label label = (Label) elevators.lookup("#Elevator" + Integer.toString(elevator) + " .speed-label");
+		Platform.runLater(() -> {
+			label.setText("Speed: " + Integer.toString(value));
+		});
+	}
+
+	public void setElevatorWeight(int elevator, int value) {
+		Label label = (Label) elevators.lookup("#Elevator" + Integer.toString(elevator) + " .weight-label");
+		Platform.runLater(() -> {
+			label.setText("Weight: " + Integer.toString(value));
+		});
+	}
+
+	public void setElevatorCapacity(int elevator, int value) {
+		Label label = (Label) elevators.lookup("#Elevator" + Integer.toString(elevator) + " .capacity-label");
+		Platform.runLater(() -> {
+			label.setText("Capacity: " + Integer.toString(value));
+		});
+	}
+
+	public void setFloorButtonUp(int floor, boolean value) {
+		Label btnUp = (Label) callButtons.lookup("#floor" + Integer.toString(floor) + " .btn-up");
+
+		if (value) {
+			btnUp.setTextFill(Color.RED);
+		} else {
+			btnUp.setTextFill(Color.BLACK);
+		}
+	}
+
+	public void setFloorButtonDown(int floor, boolean value) {
+
+		Label btnDown = (Label) callButtons.lookup("#floor" + Integer.toString(floor) + " .btn-down");
+		if (value) {
+			btnDown.setTextFill(Color.RED);
+		} else {
+			btnDown.setTextFill(Color.BLACK);
+		}
+
 	}
 
 }
---
+++ src/test/java/at/fhhagenberg/sqe/ecc/ElevatorGuiTest.java
@@ -0,0 +1,161 @@
+package at.fhhagenberg.sqe.ecc;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+@ExtendWith(ApplicationExtension.class)
+public class ElevatorGuiTest {
+	
+	private ElevatorGui gui;
+	
+	@Start
+	public void start(Stage stage) throws Exception {
+		ElevatorModel model = new ElevatorModel(3, 2, 230);
+    	
+    	
+    	FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/GUIMock.fxml"));
+        BorderPane content = loader.<BorderPane>load();
+        
+        loader.<ElevatorGui>getController().setModel(model);
+        //loader.<ElevatorGui>getController().setCo(model);
+        loader.<ElevatorGui>getController().InitGui();
+        
+        gui = loader.<ElevatorGui>getController();
+    	
+        Scene scene = new Scene(content);
+        stage.setScene(scene);
+        stage.show();
+	}
+	
+	@Test
+	public void testCommittedDirection(FxRobot robot) {
+		
+		gui.setCommitedDirection(0, IElevator.ELEVATOR_DIRECTION_DOWN);
+		
+		Label btnUp = robot.lookup("#Elevator0 .btn-up").<Label>query();
+		Label btnDown = robot.lookup("#Elevator0 .btn-down").<Label>query();
+		
+		verifyThat("#Elevator0 .btn-down", hasText("DOWN"));
+		verifyThat("#Elevator0 .btn-up", hasText("UP"));
+		assertEquals(Color.BLACK, btnUp.getTextFill());
+		assertEquals(Color.RED, btnDown.getTextFill());
+	}
+	
+	@Test
+	public void testSetTargetFloor(FxRobot robot) {
+		
+		gui.setTargetFloor(1, 1);
+		verifyThat("#Elevator1 .target-label", hasText("Target: 1"));
+	}
+	
+	@Test
+	public void testSetElevatorAccel(FxRobot robot) {
+		
+		gui.setElevatorAccel(1, 129);
+		verifyThat("#Elevator1 .accel-label", hasText("Accel: 129"));
+	}
+	
+	@Test
+	public void testElevatorButton(FxRobot robot) {
+		
+		gui.setElevatorButton(1, 1, true);
+		
+		VBox floorBox = (VBox) robot.lookup("#Elevator1 .elevator-floors").<VBox>query();
+		ObservableList<Node> floorList= floorBox.getChildren();
+		Label floor = (Label) floorList.get(floorList.size()-1-1);
+		
+		assertEquals("1", floor.getText());
+		assertEquals(Color.GREEN, floor.getTextFill());
+	}
+	
+	@Test
+	public void testSetElevatorDoorStatus(FxRobot robot) {
+		
+		gui.setElevatorDoorStatus(2, IElevator.ELEVATOR_DOORS_CLOSING);
+		verifyThat("#Elevator2 .doors-label", hasText("Doors: CLOSING"));
+	}
+	
+	@Test
+	public void testSetElevatorFloor(FxRobot robot) {
+		
+		gui.setElevatorFloor(2, 0);
+		verifyThat("#Elevator2 .floor-label", hasText("Floor: 0"));
+	}
+	
+	@Test
+	public void testSetElevatorPosition(FxRobot robot) {
+		
+		gui.setElevatorPosition(0, 120);
+		verifyThat("#Elevator0 .position-label", hasText("Position: 120"));
+	}
+	
+	@Test
+	public void testSetElevatorSpeed(FxRobot robot) {
+		
+		gui.setElevatorSpeed(1, 56);
+		verifyThat("#Elevator1 .speed-label", hasText("Speed: 56"));
+	}
+	
+	@Test
+	public void testSetElevatorWeight(FxRobot robot) {
+		
+		gui.setElevatorWeight(2, 489);
+		verifyThat("#Elevator2 .weight-label", hasText("Weight: 489"));
+	}
+	
+	@Test
+	public void testSetElevatorCapacity(FxRobot robot) {
+		gui.setElevatorCapacity(1, 12);
+		verifyThat("#Elevator1 .capacity-label", hasText("Capacity: 12"));
+	}
+	
+	@Test
+	public void testSetFloorButtonUp(FxRobot robot) {
+		
+		gui.setFloorButtonUp(0, true);
+		
+		Label button = robot.lookup("#floor0 .btn-up").<Label>query();
+		
+		verifyThat("#floor0 .btn-up", hasText("UP"));
+		assertEquals(Color.RED, button.getTextFill());
+		
+		gui.setFloorButtonUp(0, false);
+		assertEquals(Color.BLACK, button.getTextFill());
+	}
+
+	@Test
+	public void testSetFloorButtonDown(FxRobot robot) {
+		
+		gui.setFloorButtonDown(1, true);
+		
+		Label button = robot.lookup("#floor1 .btn-down").<Label>query();
+		
+		verifyThat("#floor1 .btn-down", hasText("DOWN"));
+		assertEquals(Color.RED, button.getTextFill());
+		
+		gui.setFloorButtonDown(1, false);
+		assertEquals(Color.BLACK, button.getTextFill());
+	}
+
+}
+
+
